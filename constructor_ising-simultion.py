b02f161ca1d08e750bfa94640029bdc45e36829c
45635700 <moritzhoferer@gmail.com>
2019-12-04 17:37:23 +0100

    remove unneccessary blanks

--- constructor_ising-simultion.py
+++ constructor_ising-simultion.py
@@ -8,25 +8,23 @@ import os
 import numpy as np
 import sys
 
-if (os.path.exists("parameter_files/")== False):
-	os.mkdir("parameter_files/")
+os.makedirs('parameter_files/', exist_ok=True)
 
 # system configuartion
-
 # verticle size of the system (# of rows)
-size_i 					=  int(sys.argv[1])
+size_i = int(sys.argv[1])
 
 # horizontal size of the system (# of columns)
-size_j 					=  size_i
+size_j = size_i
 
 # periodic boundary condtions in vertical direction
-periodic_bc_i 			=  1
+periodic_bc_i = 1
 
 # periodic boundary condtions in horizontal direction
-periodic_bc_j			=  1
+periodic_bc_j = 1
 
 # Boltzmann constan
-kB 						=  1
+kB = 1
 
 # temperatur
 T 						=  1
@@ -35,43 +33,43 @@ T 						=  1
 # system properties
 
 # part of particles of type A / +1
-part_A 					=  .125 * (size_i*size_j) - int(sys.argv[2])
+part_A = .125 * (size_i*size_j) - int(sys.argv[2])
 
 # part of particles of type B / -1
-part_B 					=  .875 * (size_i*size_j)
+part_B = .875 * (size_i*size_j)
 
 # part of particles of type O / 0
-part_O					=  int(sys.argv[2])
+part_O = int(sys.argv[2])
 
 # energy of to neighbouring particles of type A
-E_AA 					=  -1
+E_AA = -1
 
 # energy of to neighbouring particles of type B
-E_BB 					=  -1 # E_AA
+E_BB = -1 # E_AA
 
 # energy of to neighbouring particles of type O
-E_OO					=  0 
+E_OO = 0 
 
 # energy of to neighbour particles one with type A and one with type B
-E_AB 					=  1 # -E_AA
+E_AB = 1 # -E_AA
 
 # energy of to neighbour particles one with type A and one with type O
-E_AO 					=  0 # -E_OO
+E_AO = 0 # -E_OO
 
 # energy of to neighbour particles one with type B and one with type O
-E_BO 					=  0 #  E_OO
+E_BO = 0 #  E_OO
 
 
 
 # external may automatically positioned in the middle of the lattice
 # external field on or off
-ext_field_flag			= 0
+ext_field_flag = 0
 
 # max value
-ext_field_max_value		= 0 
+ext_field_max_value	= 0 
 
 # homogenous ( = 0 ) or with decay ( > 0 )
-ext_field_decay			= 0 
+ext_field_decay	= 0 
 
 
 # simulation properties
@@ -80,25 +78,25 @@ seed 					= 13
 np.random.seed(seed)
 
 # selection methode: if =1 Glauber and if = 0 Metropolis is used
-GlauberProb 			= 1
+GlauberProb = 1
 
 # maximum number of executed simulations steps
 # pow(10, max_step_count) * size_i * size_j || JUST ENTER THE EXPONENT
-max_step_count		 	= int(4)
+max_step_count = int(4)
 
 # number of simulation steps to executed before the average occupation recording is activated
 # pow(10, burn_in_count) * size_i * size_j || JUST ENTER THE EXPONENT
-burn_in_count			= int(6)
+burn_in_count = int(6)
 
 # number of simulation steps to pass by untill the next snap shot (used for total energy and the number of different bounds)
 # pow(10, diff_count) * size_i * size_j || JUST ENTER THE EXPONENT
 diff_count				= int(0)
 
 # used for angle and distance dependent distribution function ( 0 off, if > 0 cut off radius )
-record_Density			= 10
+record_Density = 10
 
 # the total energy and the number of different bound is recorded
-record_Energy			= 0
+record_Energy = 0
 
 
 # choosen : "E_BO"
@@ -130,18 +128,13 @@ else:
 	
 
 file= open('./parameter_files/option.txt','w+')
-
 for i in range(len(var)*len(var2)):
-
 	file.write('{} \n'.format(i+1))
 
 file.close()
 
 c = 1
-
-
 for j in range(len(var2)):
-
 	# !!! var2 to be changed !!!
 	#T = var2[j]
 	E_AA = -var2[j]
@@ -149,9 +142,7 @@ for j in range(len(var2)):
 	E_AB =  var2[j]
 	#E_BO =  var2[j]
 
-
 	for i in range(len(var)):
-
 		# !!! var to be changed !!!
 		#E_AA = -var2[i]
 		#E_BB = -var2[i]
@@ -199,5 +190,4 @@ for j in range(len(var2)):
 		file.write('{}'.format(record_Energy))
 
 		file.close()
-
 		c += 1
